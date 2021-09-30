ce50dac75ac37c11ac6bf12a0a0284a1374ee91c
keithwong2018 <keith.wong.2018@sis.smu.edu.sg>
2021-09-30 13:26:53 +0800

    Added select to filter by category

--- src/components/Products.js
+++ src/components/Products.js
@@ -3,23 +3,35 @@ import categories from "../data/categories.json";
 import products from "../data/products.json";
 import "../css/products.css";
 import Select from "react-select";
+import { useState, useEffect } from "react";
 
 function Products() {
-  const listItems = products.map((item) => (
-    <div className="card" key={item.id}>
-      <div className="card_img">
-        <img src={item.image} />
-      </div>
-      <div className="card_header">
-        <h2>{item.title}</h2>
+  const [selId, setCatId] = useState("");
+  console.log(selId);
 
-        <p className="description">{item.description}</p>
-        <p className="price">${item.price}</p>
-        <p className="qty">Quantity Left: {item.qty}</p>
+  function ddlHandler(e) {
+    setCatId(e.value);
+  }
+
+  const listItems = products.map((item) =>
+    item.category_id === selId ? (
+      <div className="card" key={item.id}>
+        <div className="card_img">
+          <img src={item.image} />
+        </div>
         <div className="btn">Add to cart</div>
+        <div className="card_header">
+          <h2>{item.title}</h2>
+
+          {/* <p className="description">{item.description}</p> */}
+          <p className="price">${item.price}</p>
+          <p className="qty">Quantity Left: {item.qty}</p>
+        </div>
       </div>
-    </div>
-  ));
+    ) : (
+      ""
+    )
+  );
 
   const options = categories.map((category) => ({ value: category.id, label: category.name }));
 
@@ -27,7 +39,7 @@ function Products() {
     <div className="main_content">
       <div className="product-header">
         <h3>
-          <Select options={options} />
+          <Select options={options} onChange={ddlHandler} />
         </h3>
       </div>
       {listItems}
